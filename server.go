4219cef917a2024ea22160f5bd3e21192f492030
Evan Victor <evan.vp@inapp.com>
2023-10-25 16:41:31 +0530

    pages added and transition

--- server.go
+++ server.go
@@ -9,20 +9,33 @@ import (
 func main() {
 	// The handler function that will be called for all requests.
 	// It will load the template and execute it.
-	handleHome := func(w http.ResponseWriter, r *http.Request) {
+	handleIndex := func(w http.ResponseWriter, r *http.Request) {
 		template := template.Must(template.ParseFiles("views/index.html"))
 		template.Execute(w, nil)
 	}
 
-	handleClicked := func(w http.ResponseWriter, r *http.Request) {
-		template := template.Must(template.ParseFiles("views/clicked.html"))
+	handleHome := func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("views/modules/home/home.html"))
+		template.Execute(w, nil)
+	}
+
+	handleLogs := func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("views/modules/logs/logs.html"))
+		template.Execute(w, nil)
+	}
+
+	handleAbout := func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("views/modules/about/about.html"))
 		template.Execute(w, nil)
 	}
 
 	// Associate the handler function with the root URL.
-	http.HandleFunc("/", handleHome)
-	http.HandleFunc("/clicked", handleClicked)
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/logs", handleLogs)
+	http.HandleFunc("/about", handleAbout)
+	http.HandleFunc("/home", handleHome)
 
 	// Start the server.
+	log.Println("Running on http://localhost:8080/")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
